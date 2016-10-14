cd18cdfa64726c4a5e865d37a5baa7fb56daf247
Jhonata Menezes <jhonatamenezes10@gmail.com>
2016-10-13 23:40:00 -0300

    trabalhando com filtros

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
  a = new Vue({  
   el: '#app',
   data: {
-    titulo: 'So Promocao',
-    lista: [
-    	{nome: "Jhonata"},
-    	{nome: "David"},
-    	{nome: "Mae"},
-    	{nome: "Mariana"}
-    ]
+     produtos:[],
+     search:''
+  },
+
+  methods:{
+  },
+  computed:{
+    filtroSearch:function(){
+      var self=this;
+      return self.produtos.filter(function (p) {
+        console.log(p.nome);
+        return p.nome.toLowerCase().indexOf(self.search.toLowerCase()) !== -1
+      });
+    }
+  },
+
+  mounted:function(){
+    var self=this;
+    self.$http.get('/produtos.json').then(function(r, e){
+      if(r){
+        self.produtos = r.body;
+      }
+    });
   }
-});
+ });
