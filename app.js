30951ec24d6d4322d398b6363f170532ce90302b
Jhonata Menezes <jhonatamenezes10@gmail.com>
2016-10-14 18:02:29 -0300

    paginacao

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-a = new Vue({  
-  el: '#app',
-  data: {
-     produtos:[],
-     search:''
-  },
-
-  methods:{
-  },
-  computed:{
-    filtroSearch:function(){
-      var self=this;
-      return self.produtos.filter(function (p) {
-        return p.nome.toLowerCase().indexOf(self.search.toLowerCase()) !== -1
-      });
+a = new Vue({
+    el: '#app',
+    data: {
+        produtos: [],
+        produtosOrder: 'asc',
+        search: ''
     },
-    orderByColumn: function(){
-      return this.produtos;
-    }
-  },
 
-  mounted:function(){
-    var self=this;
-    self.$http.get('/produtos.json').then(function(r, e){
-      if(r){
-        self.produtos = r.body;
-      }
-    });
-  }
- });
+    methods: {
+        orderByColumn: function(event, col) {
+            event.preventDefault();
+            if (this.produtosOrder == 'asc') {
+                this.produtosOrder = 'desc';
+            } else {
+                this.produtosOrder = 'asc';
+            }
+            this.produtos = _.orderBy(this.produtos, col, this.produtosOrder);
+        }
+    },
+
+    computed: {
+        filtroSearch: function() {
+            var self = this;
+            return self.produtos.filter(function(p) {
+                return p.nome.toLowerCase().indexOf(self.search.toLowerCase()) !== -1
+            });
+        }
+    },
+
+    mounted: function() {
+        var self = this;
+        self.$http.get('/produtos.json').then(function(r, e) {
+            if (r) {
+                self.produtos = r.body;
+            }
+        });
+    }
+});
