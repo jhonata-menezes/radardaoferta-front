0c9418c7364dd9bc63e56a4a662998f643d6ad11
Jhonata Menezes <jhonatamenezes10@gmail.com>
2016-11-04 18:06:38 -0200

    inserindo paginacao

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ a = new Vue({
     el: '#app',
     data: {
         produtos: [],
+        produtosCompleto: [],
         produtosOrder: 'asc',
         search: '',
-        url: ''
+        url: '',
+        paginator: {
+            paginaAtual: 1,
+            qtdItensPorPagina: 50,
+            qtdIndicesPagina: 4
+        }
     },
-
     methods: {
         orderByColumn: function(event, col) {
             event.preventDefault();
@@ -20,12 +25,32 @@ a = new Vue({
         novaUrl: function(event) {
             event.preventDefault();
             var self = this;
-            self.$http.post('http://127.0.0.1:5014/api/produtos/novo', { url: self.url }).then(function(r, e) {
+            self.$http.post('http://192.168.56.111:5014/api/produtos/novo', { url: self.url }).then(function(r, e) {
                 if (r) {
                     document.getElementById('openModalNovaUrlOK').click();
                     self.url = '';
                 }
             });
+        },
+        paginacao: function(e, p) {
+            e.preventDefault();
+            this.paginator.paginaAtual = p;
+        },
+        anterior: function(e) {
+            e.preventDefault();
+            this.paginator.paginaAtual--;
+
+        },
+        proximo: function(e) {
+            e.preventDefault();
+            this.paginator.paginaAtual++;
+        },
+        qtdTotalItens: function() {
+            return this.produtosCompleto.length;
+        },
+        atualizaQuantidadeLista: function(e) {
+            var inicio = (this.qtdTotalItens() * (this.paginator.paginaAtual - 1));
+            this.produtos = this.produtosCompleto.slice(inicio, this.paginator.qtdItensPorPagina);
         }
     },
 
@@ -35,14 +60,28 @@ a = new Vue({
             return self.produtos.filter(function(p) {
                 return p.nome.toLowerCase().indexOf(self.search.toLowerCase()) !== -1
             });
+        },
+        qtdItensPorPaginaComputed: {
+            get: function() {
+                return this.paginator.qtdItensPorPagina;
+            },
+            set: function(v) {
+                this.paginator.qtdItensPorPagina = v;
+                this.atualizaQuantidadeLista();
+            }
+        },
+
+        qtdPaginasClicaveis: function() {
+            return [1, 2, 3];
         }
     },
 
     mounted: function() {
         var self = this;
-        self.$http.get('http://127.0.0.1:5014/api/produtos').then(function(r, e) {
+        self.$http.get('http://192.168.56.111:5014/api/produtos').then(function(r, e) {
             if (r) {
-                self.produtos = r.body;
+                self.produtosCompleto = r.body;
+                self.produtos = self.produtosCompleto.slice(0, self.paginator.qtdItensPorPagina);
             }
         });
     }
