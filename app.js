57c5f4eb04a8fd69c57a2d931b464f1c71447372
Jhonata Menezes <jhonatamenezes10@gmail.com>
2016-11-28 16:22:38 -0200

    nao funciona

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var Vue = require('vue')
 var VueRouter = require('vue-router')
 var App = require('./App.vue')
 
-Vue.config.devtools = false; //desabilita mensagem no browser
 Vue.use(VueRouter)
 
 routes = []
